01bc7df7642ae474d2b5f44a238feef20e42a5ee
xiongcha <unlaytsab@gmail.com>
2024-07-24 17:22:33 +0700

    Update all

--- app/controllers/admin/StudentScoreController.php
+++ app/controllers/admin/StudentScoreController.php
@@ -12,18 +12,33 @@ class StudentScoreController{
         $tmp = new Template;
         $custom = new CustomFunctions();
         $help = new HelpFunctions();
-        $Svr = new SubjectServices($this->db);
         $items = $this->db->exec("SELECT * FROM tblstudent ORDER BY student_no");
         $f3->set('items',$items);
         $f3->set('help',$help);
         $f3->set('entrycount',count($items));
         $f3->set('province',$custom->province());
-        $f3->set('semester',$custom->semester());
         $f3->set('arrClass',$custom->arrClass());
         $f3->set('nav','student-score');
         $f3->set('subnav','student-score');
         $f3->set('strAction', 'ໃຫ້ຄະແນນນັກສຶກສາ');
         $f3->set('strPage', 'ນັກສຶກສາ');
         echo $tmp->render("backend/student-score.html");
+    }
+    function score(){
+        $f3 = Base::instance();
+        $Svr = new ScoreServices($this->db);
+        $semester = $f3->get('PARAMS.semester');
+        $student_no = $f3->get('PARAMS.student_no');
+        $item = $Svr->load(array('student_no = ? AND semester = ?',$student_no,$semester));
+        if($item){
+            API::success(array('success' => true, 'score' => $item->score));
+        } else {
+            API::success(array('success' => true, 'score' => ''));
+        }
+    }
+
+    function storeData(){
+        $Svr = new ScoreServices($this->db);
+        $Svr->add();
     }
 }
