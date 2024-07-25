5744486e90b3c9ce8e95a3281b27eeeb6cc01521
KA <katrade1997@gmail.com>
2024-07-25 13:45:39 +0700

    ka updated

--- app/controllers/admin/RegisterController.php
+++ app/controllers/admin/RegisterController.php
@@ -10,6 +10,7 @@ class RegisterController extends ActionController {
         $this->secur = new CustomSecurity();
         $this->secur->security($this->db);
         $this->custom = new CustomFunctions();
+        $this->help = new HelpFunctions();
        
         $this->f3->set('arrProvince',$this->custom->province());
         $this->f3->set('arrClass',$this->custom->arrClass());
@@ -17,13 +18,17 @@ class RegisterController extends ActionController {
         $this->f3->set('custom',$this->custom);
 	    parent::__construct('RegisterServices', 'backend/register.html', 'register', 'register', 'ລົງທະບຽນນັກສຶກສາໃໝ່');
     }
-    function getDistrict(){
-        $disSvr = new DistrictServices($this->db);
-        $arrDis=[];
-        $disItems = $disSvr->getAll(['province_id=?',$this->f3->get('PARAMS.province')],[]);
-        foreach ($disItems as $value) {
-            $arrDis[$value['id']] = $value['district_name'];
+
+    function getDistrict()
+    {
+        $province_id = $this->f3->get('PARAMS.province');
+        $items       = $this->help->getFind('DistrictServices',array(' province_id = ? ',$province_id));
+        $arr         = array();
+        foreach($items as $row)
+        {
+            $arr[] = array('id' => $row['id'], 'province_id' => $row['province_id'], 'district_name' => $row['district_name']);
         }
-        API::success(['success'=>true,'data'=>$arrDis]);
+        $this->data = array('success' => true, 'data' => $arr);
+        API::success($this->data);
     }
 }
